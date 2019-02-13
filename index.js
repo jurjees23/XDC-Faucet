faeba3130363670e7f9a291c1747a461bfcecaaf
rushabh parmar <43805620+parmarrushabh@users.noreply.github.com>
2019-02-13 14:29:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,14 +29,14 @@ require('./helpers/configHelper')(app);
 require('./helpers/blockchainHelper')(app);
 require('./helpers/captchaHelper')(app);
 require('./controllers/index')(app);
+require('./controllers/getTxCallBack')(app);
 
 app.get('/', function(request, response) {
-  response.send('Sokol POA Network faucet');
+  response.send('Xinfin testnet  faucet');
 });
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function () {
-    console.log('Sokol testnet POA Network faucet is running on port', app.get('port'));
+    console.log('Xinfin Testnet faucet is running on port', app.get('port'));
 });
-
