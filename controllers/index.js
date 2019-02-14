8b47ef2bc6917b7ca8a774f08874f6e59119d860
parmarrushabh <rushabhparmar17@gmail.com>
2019-02-14 14:42:06 +0530

    updated index.js

--- controllers/index.js
+++ controllers/index.js
@@ -27,4 +27,45 @@ module.exports = function (app) {
 	function configureWeb3Response(err, web3, receiver, response) {
 		if (err) return generateErrorResponse(response, err);
 
-		.	
+		var senderPrivateKey = config.Ethereum[config.environment].privateKey;
+		const privateKeyHex = Buffer.from(senderPrivateKey, 'hex')
+		if (!web3.isAddress(receiver)) return generateErrorResponse(response, {code: 500, title: "Error", message: "invalid address"});
+		
+		var gasPrice = parseInt(web3.eth.gasPrice);
+		var gasPriceHex = web3.toHex(gasPrice);
+		var amount = parseInt(web3.toWei(config.Ethereum.etherToTransfer, "ether"));
+		var nonce = web3.eth.getTransactionCount(config.Ethereum[config.environment].account);
+		var nonceHex = web3.toHex(nonce);
+		const rawTx = {
+		  nonce: nonceHex,
+		  gasPrice: gasPriceHex,
+		  gasLimit: config.Ethereum.gasLimit,
+		  to: receiver, 
+		  value: web3.toHex(amount),
+		  data: '0x00',
+		  chainId: web3.toHex(web3.version.network)
+		};
+		
+		var tx = new EthereumTx(rawTx);
+		tx.sign(privateKeyHex);
+
+		var serializedTx = tx.serialize();
+
+		web3.eth.sendRawTransaction("0x" + serializedTx.toString('hex'), function(err, hash) {
+			sendRawTransactionResponse(err, hash, response);
+		});
+	}
+
+	function sendRawTransactionResponse(err, hash, response) {
+		if (err) return generateErrorResponse(response, err);
+
+		var successResponse = {
+			code: 200, 
+			title: "Success", 
+			message: "Tx is posted to blockchain",
+			txHash: hash
+		};
+	  	
+		response.send({ success: successResponse });
+	}
+}
