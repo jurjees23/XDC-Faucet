8f91b7ef65f416d93763a44e62660c4b26706b20
nakraniprakash <mr.prakashnakrani@gmail.com>
2019-03-02 17:21:48 +0530

    Fixed validation issue and modal issue on api call

--- public/assets/javascripts/application/index.js
+++ public/assets/javascripts/application/index.js
@@ -1,8 +1,10 @@
 $(function() {
 	var loader = $(".loading-container");
-	$( "#faucetForm" ).submit(function( e ) {
+	$("#requestTokens").on('click', function( e ) {
 		e.preventDefault();
-    	$this = $(this);
+		$('#modalSuccess').hide();
+		$('#modalError').hide();
+		$this = $(this);
 		loader.removeClass("hidden");
 		var receiver = $("#receiver").val();
     var captcha =  $("#g-recaptcha-response").val();
@@ -14,22 +16,30 @@ $(function() {
           captcha: captcha
 		  	}
 		}).done(function(data) {
+			console.log(data);
 			if (!data.success) {
 				loader.addClass("hidden");
-				swal(data.error.title, data.error.message, "error");
+				$('#modalSuccess').hide();
+				$('#modalError').show();
+				$('#modalErrorText').html(data.error.message);
+				// swal(data.error.title, data.error.message, "error");
 				return;
+			} else {
+				$('#modalSuccess').show();
+				$('#modalError').hide();
 			}
 
 			getTxCallBack(data.success.txHash, function() {
 				$("#receiver").val('');
 				loader.addClass("hidden");
-				swal("Success",
-				  "15 TOMO is successfully transfered to " + receiver +" in Tx<br /><a href='https://scan.testnet.tomochain.com/txs/" + data.success.txHash + "' target='_blank'>" + data.success.txHash + "</a>",
-				  "success"
-				);
+				// swal("Success",
+				//   "15 TOMO is successfully transfered to " + receiver +" in Tx<br /><a href='https://scan.testnet.tomochain.com/txs/" + data.success.txHash + "' target='_blank'>" + data.success.txHash + "</a>",
+				//   "success"
+				// );
 				grecaptcha.reset();
 			});
 		}).fail(function(err) {
+			console.log('err', err);
 			loader.addClass("hidden");
 		});
 	});
